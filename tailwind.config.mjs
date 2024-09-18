554581593e6549cd3b8e04a7eb03c3113df73dc8
DrMaul <agusfmartinez99@gmail.com>
2024-09-18 13:57:39 -0300

    set custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				customBlue: '#30475e',
+				customGray: '#7e8a97',
+				customBrown: '#cbaf87',
+				customLigth: '#e7dec8'
+			}
+		},
 	},
 	plugins: [],
 }
